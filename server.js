d653227ae4b0af6a359620847efaebb0a6c7202a
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-01-10 17:48:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+dotenv.config()
+const app= express()
+app.use(cors())
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Society Fund Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
