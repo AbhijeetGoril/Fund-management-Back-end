e3bab06d2de6fc8473f692e0372e943bd4c92b51
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-01-12 21:33:40 +0530

    add the firebase middleware in server js file

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import connectDB from "./config/db.js"
-dotenv.config()
-const app= express()
-app.use(cors())
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import firebaseAuth from "./middleware/firebaseAuth.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Society Fund Backend Running 🚀");
 });
 
+// 🔐 Protected route
+app.get("/api/protected", firebaseAuth, (req, res) => {
+  res.json({
+    message: "User verified by Firebase ✅",
+    uid: req.user.uid,
+    email: req.user.email,
+  });
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  connectDB()
-})
+  connectDB();
+});
