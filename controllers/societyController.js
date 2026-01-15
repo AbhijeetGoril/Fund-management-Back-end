96edbc1fedf0d1f1b2bbc03ce90ee740881ae1d8
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-01-15 21:05:13 +0530

    add createEvent API and corresponding route

--- controllers/societyController.js
+++ controllers/societyController.js
@@ -1,6 +1,7 @@
 
 import User from "../models/User.js";
 import Society from "../models/Society.js";
+import Event from "../models/Event.js";
 
 export const createSociety = async (req, res) => {
   try {
@@ -24,6 +25,44 @@ export const createSociety = async (req, res) => {
       ],
     })
     res.status(201).json(society);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const createEvent = async (req, res) => {
+  try {
+    const { title,date,description,societyId } = req.body;
+    if (!title || !description || !societyId) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // find logged-in user
+    const dbUser = await User.findOne({
+      firebaseUid: req.user.uid,
+    });
+
+    if (!dbUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+     // check society
+    const society = await Society.findById(societyId);
+
+    if (!society) {
+      return res.status(404).json({ message: "Society not found" });
+    }
+
+    const isAdmin=society.members.some(m=>m.user.toString()===dbUser._id.toString() &&  m.role === "admin")
+    if(!isAdmin){
+      return res.status(403).json({ message: "Admin access only" });
+    }
+    const event= await Event.create({
+      title,
+      date,
+      description,
+      society: society._id,
+      createdBy: dbUser._id
+    })
+    res.status(201).json(event);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routes/societyRoutes.js
+++ routes/societyRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import firebaseAuth from "../middleware/firebaseAuth.js"
-import { createSociety } from "../controllers/societyController.js"
+import { createSociety,createEvent } from "../controllers/societyController.js"
 const router=express.Router()
 router.post("/createSociety",firebaseAuth,createSociety)
+router.post("/createEvent",firebaseAuth,createEvent)
 export default router
