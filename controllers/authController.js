82a4028c24a0fea8b065ee2d145c651fab4a7802
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-01-17 15:37:27 +0530

    implement sendOtp function to generate and hash OTP; add bcrypt dependency

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
-export const sendOtp async =(req,res)=>{
+import bcrypt from "bcryptjs"
+
+export const sendOtp  = async (req,res)=>{
   try {
-    
+    const {email}=req.body
+    if (!email) {
+      return res.status(400).json({ message: "Email is required" });
+    }
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const otpHash = await bcrypt.hash(otp, 10);
   } catch (error) {
     
   }
