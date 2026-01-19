c506c57abf325fc435bfa320ac97bda355d91849
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-01-19 18:53:03 +0530

    fix import paths in authRoutes.js and societyRoutes.js for correct module resolution

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { googleAuth, sendOtp,setPassword,verifyOtp } from "../controllers/authController.js";
+import { googleAuth, sendOtp,setPassword,verifyOtp } from "../../controllers/authController.js";
 const router=express.Router()
 router.post("/send-otp",sendOtp)
 router.post("/verify-otp", verifyOtp);
--- routes/societyRoutes.js
+++ routes/societyRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
-import firebaseAuth from "../middleware/firebaseAuth.js"
-import { createSociety,createEvent } from "../controllers/societyController.js"
+import firebaseAuth from "../../middleware/firebaseAuth.js"
+import { createSociety,createEvent } from "../../controllers/societyController.js"
 const router=express.Router()
 router.post("/createSociety",firebaseAuth,createSociety)
 router.post("/createEvent",firebaseAuth,createEvent)
