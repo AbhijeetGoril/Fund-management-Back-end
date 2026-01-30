befdab4041344669a59f0f8a1051fef224a0a121
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-01-30 21:01:42 +0530

    add the createdBy property in societySchema

--- models/Society.js
+++ models/Society.js
@@ -1,27 +1,41 @@
 import mongoose from "mongoose";
-const societySchema=new mongoose.Schema({
-  name:{
-    type:String,
-    required: true,
-    trim: true,
-  },
-  members: [
+
+const societySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    members: [
       {
         user: {
           type: mongoose.Schema.Types.ObjectId,
           ref: "User",
           required: true,
         },
+
         role: {
           type: String,
           enum: ["admin", "member"],
           default: "member",
         },
+
         joinedAt: {
           type: Date,
           default: Date.now,
         },
       },
     ],
-},{ timestamps: true })
+  },
+  { timestamps: true }
+);
+
 export default mongoose.model("Society", societySchema);
