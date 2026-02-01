4419747bd8f5a7364981ded6e8366d7b92b25418
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-02-01 20:33:20 +0530

    change the eventMemberSchema

--- models/Event/EventMember.js
+++ models/Event/EventMember.js
@@ -1,2 +1,30 @@
+// models/EventMember.js
 import mongoose from "mongoose";
-const eventMemberSchema = new mongoose.Schema({},{timestamps:true})
+
+const eventMemberSchema = new mongoose.Schema(
+  {
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true, 
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "member"],
+      default: "member",
+    },
+  },
+  { timestamps: true }
+);
+
+// one user can have only one role per event
+eventMemberSchema.index({ event: 1, user: 1 }, { unique: true });
+
+export default mongoose.model("EventMember", eventMemberSchema);
