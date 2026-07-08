c2daa87ab991d1df6b2f0edb949d381c0e1be8a8
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-07-08 09:04:28 +0530

    feat: remove addParticipant route and related controller logic

--- routes/societyRoutes.js
+++ routes/societyRoutes.js
@@ -1,13 +1,13 @@
 import express from "express"
 import firebaseAuth from "../middleware/firebaseAuth.js"
-import { createSociety,createEvent, getAllMyRelatedEvents, addParticipant, getSingleEvent, addSpend, updateSpend, deleteSpend, getEventSpends } from "../controllers/societyController.js"
+import { createSociety,createEvent, getAllMyRelatedEvents, getSingleEvent, addSpend, updateSpend, deleteSpend, getEventSpends } from "../controllers/societyController.js"
 import { authMiddleware } from "../middleware/authMiddleware.js"
 import upload from "../middleware/upload.js"
 const router=express.Router()
 router.post("/createSociety",authMiddleware,createSociety)
 router.get("/events/allMyRelatedEvents",authMiddleware,getAllMyRelatedEvents)
 router.post("/events/createEvent",authMiddleware,upload.single("coverPhoto"),createEvent)
-router.post("/events/addParticipant",authMiddleware,addParticipant)
+// router.post("/events/addParticipant",authMiddleware,addParticipant)
 router.get(
   "/events/:eventId",
   authMiddleware,
