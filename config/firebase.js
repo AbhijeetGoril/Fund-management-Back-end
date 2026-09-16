b180fe827beaa5ef75556c63e41d2752656c3fd5
AbhijeetGoril <abhijeetgorilz@gmail.com>
2026-09-16 11:39:10 +0530

    fix: refactor service account initialization in firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,20 @@
-import admin from"firebase-admin"
-import fs from "fs"
+import admin from "firebase-admin";
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "firebase-admin.json"), "utf8")
-);
+
+let serviceAccount;
+
+if (process.env.FIREBASE_ADMIN_JSON) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_JSON);
+} else {
+  serviceAccount = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "firebase-admin.json"), "utf8")
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
